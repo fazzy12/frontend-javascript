91a50f60c513a45fd8bc7bbaa7723df66620445b
ifeanyi kalu <kaluifeanyi00@gmail.com>
2025-09-18 11:03:31 +0100

    build a Teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,27 +1,9 @@
 interface Teacher {
-    readonly firstName : string;
-    readonly lastname : string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [propName: string]: any;
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  contract: boolean;
+  [propName: string]: any;
 }
-
-const teacher1: Teacher = {
-    firstName: "jane",
-    lastname: "kalu",
-    fullTimeEmployee: true,
-    yearsOfExperience: 5,
-    location: "Abuja"
-}
-
-const teacher2 : Teacher = {
-    firstName: "bee",
-    lastname: "vera",
-    fullTimeEmployee: false,
-    location: "Aba",
-    contract: true,
-}
-
-console.log(teacher1)
-console.log(teacher2)
